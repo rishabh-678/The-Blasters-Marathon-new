c1e1ab7b001dfa833030f71068936635e7ed2082
rishabh-678 <negirishabh454@gmail.com>
2026-02-09 18:31:59 +0530

    update all changes

--- assets/js/custom.js
+++ assets/js/custom.js
@@ -1,90 +1,91 @@
-jQuery( document ).ready(function( $ ) {
+jQuery(document).ready(function ($) {
+  "use strict";
 
+  // Page loading animation
 
-	"use strict";
+  $("#preloader").animate(
+    {
+      opacity: "0",
+    },
+    600,
+    function () {
+      setTimeout(function () {
+        $("#preloader").css("visibility", "hidden").fadeOut();
+      }, 300);
+    },
+  );
 
-        // Page loading animation
+  $(window).scroll(function () {
+    var scroll = $(window).scrollTop();
+    var box = $(".header-text").height();
+    var header = $("header").height();
 
-        $("#preloader").animate({
-            'opacity': '0'
-        }, 600, function(){
-            setTimeout(function(){
-                $("#preloader").css("visibility", "hidden").fadeOut();
-            }, 300);
-        });
-        
+    if (scroll >= box - header) {
+      $("header").addClass("background-header");
+    } else {
+      $("header").removeClass("background-header");
+    }
+  });
 
-        $(window).scroll(function() {
-          var scroll = $(window).scrollTop();
-          var box = $('.header-text').height();
-          var header = $('header').height();
+  if ($(".owl-clients").length) {
+    $(".owl-clients").owlCarousel({
+      loop: true,
+      nav: false,
+      dots: true,
+      items: 1,
+      margin: 30,
+      autoplay: false,
+      smartSpeed: 700,
+      autoplayTimeout: 6000,
+      responsive: {
+        0: {
+          items: 1,
+          margin: 0,
+        },
+        460: {
+          items: 1,
+          margin: 0,
+        },
+        576: {
+          items: 2,
+          margin: 20,
+        },
+        992: {
+          items: 3,
+          margin: 30,
+        },
+      },
+    });
+  }
 
-          if (scroll >= box - header) {
-            $("header").addClass("background-header");
-          } else {
-            $("header").removeClass("background-header");
-          }
-        });
-        
-        if ($('.owl-clients').length) {
-            $('.owl-clients').owlCarousel({
-                loop: true,
-                nav: false,
-                dots: true,
-                items: 1,
-                margin: 30,
-                autoplay: false,
-                smartSpeed: 700,
-                autoplayTimeout: 6000,
-                responsive: {
-                    0: {
-                        items: 1,
-                        margin: 0
-                    },
-                    460: {
-                        items: 1,
-                        margin: 0
-                    },
-                    576: {
-                        items: 2,
-                        margin: 20
-                    },
-                    992: {
-                        items: 3,
-                        margin: 30
-                    }
-                }
-            });
-        }
-
-        if ($('.owl-banner').length) {
-            $('.owl-banner').owlCarousel({
-                loop: true,
-                nav: false,
-                dots: true,
-                items: 1,
-                margin: 0,
-                autoplay: false,
-                smartSpeed: 700,
-                autoplayTimeout: 6000,
-                responsive: {
-                    0: {
-                        items: 1,
-                        margin: 0
-                    },
-                    460: {
-                        items: 1,
-                        margin: 0
-                    },
-                    576: {
-                        items: 1,
-                        margin: 20
-                    },
-                    992: {
-                        items: 1,
-                        margin: 30
-                    }
-                }
-            });
-        }
+  if ($(".owl-banner").length) {
+    $(".owl-banner").owlCarousel({
+      loop: true,
+      nav: false,
+      dots: true,
+      items: 1,
+      margin: 0,
+      autoplay: false,
+      smartSpeed: 700,
+      autoplayTimeout: 6000,
+      responsive: {
+        0: {
+          items: 1,
+          margin: 0,
+        },
+        460: {
+          items: 1,
+          margin: 0,
+        },
+        576: {
+          items: 1,
+          margin: 20,
+        },
+        992: {
+          items: 1,
+          margin: 30,
+        },
+      },
+    });
+  }
 });
